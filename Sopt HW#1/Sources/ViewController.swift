9fe8bdf8704369e13f4b5d327eeb8b435de96c20
Junnegeem <nw53@naver.com>
2019-10-04 23:01:41 +0900

    add first assignment

---
+++ Sopt HW#1/Sources/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Sopt HW#1
+//
+//  Created by JUNE on 2019. 10. 3..
+//  Copyright © 2019년 JUNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var joayo: UIButton!
+    @IBAction func heart(_sender: UIButton){
+        
+        
+        
+    }
+    
+        override func   viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
